bd53ada0022370c0effc8ff669dbebdf94e11222
caorpr <caorpr@gmail.com>
2016-01-06 11:59:41 -0700

    Initial Commit

---
+++ AbotMeApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AbotMeApp2
+//
+//  Created by Anderson, Clayton on 1/6/16.
+//  Copyright © 2016 Anderson, Clayton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
