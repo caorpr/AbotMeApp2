83256032211739a6901b8d0f721d7c35dfaaba0b
caorpr <caorpr@gmail.com>
2016-01-06 12:31:15 -0700

    Made Screens and added screen switching

---
+++ AbotMeApp2/BoardGameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BoardGameViewController.swift
+//  AbotMeApp2
+//
+//  Created by Anderson, Clayton on 1/6/16.
+//  Copyright © 2016 Anderson, Clayton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BoardGameController : UIViewController
+{
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
